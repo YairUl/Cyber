f3b9418108c03fd4fbb83f015b290919e3d8598b
yairu <yair.uliel2@gmail.com>
2024-11-13 14:39:41 +0200

    tree 2

--- Binary tree Hw/binary_tree.py
+++ Binary tree Hw/binary_tree.py
@@ -19,13 +19,13 @@ def tree_coontain(root, string):
             if tree_count(root, letter) == 0:
                 return False
     return True
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
+#root = Node(1)
+#root.left = Node(2)
+#root.right = Node(3)
+#root.left.left = Node(4)
+#root.left.right = Node(5)
+#root.right.left = Node(6)
+#root.right.right = Node(7)
 
 #print(tree_count(root, 4))
-print(tree_coontain(root, 357))
+#print(tree_coontain(root, 357))
